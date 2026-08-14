c7d403d2c6fff8ca5985106115c65d31bcd2a505
Nicolas Gasull <nicolas@gasull.net>
2026-08-15 01:22:11 +0200

    ItemRarityUI: Add rarity BG for new scales

--- IconsInventory_ItemRarityUI/gen.js
+++ IconsInventory_ItemRarityUI/gen.js
@@ -1,19 +1,23 @@
-function gen(scaling) {
-    let canvas = document.createElement("canvas")
-    canvas.width = 1
-    canvas.height = 40 * scaling
-    let ctx = canvas.getContext("2d")
-    let radgrad = ctx.createLinearGradient(0, canvas.height, 0, 0);
-    radgrad.addColorStop(0, 'rgba(255,255,255,1)');
-    radgrad.addColorStop(0.25, 'rgba(255,255,255,0.65)');
-    radgrad.addColorStop(1, 'rgba(255,255,255,0)');
-    ctx.fillStyle = radgrad;
-    ctx.fillRect(0, 0, 1, canvas.height)
-    let a = document.createElement("a")
-    a.href = canvas.toDataURL("image/png")
-    a.download = `rarity-bg-${scaling}.png`
-    a.dispatchEvent(new MouseEvent("click"))
-}
+(async () => {
+    function gen(scaling) {
+        let canvas = document.createElement("canvas")
+        canvas.width = 1
+        canvas.height = 40 * scaling
+        let ctx = canvas.getContext("2d")
+        let radgrad = ctx.createLinearGradient(0, canvas.height, 0, 0);
+        radgrad.addColorStop(0, 'rgba(255,255,255,1)');
+        radgrad.addColorStop(0.25, 'rgba(255,255,255,0.65)');
+        radgrad.addColorStop(1, 'rgba(255,255,255,0)');
+        ctx.fillStyle = radgrad;
+        ctx.fillRect(0, 0, 1, canvas.height)
+        let a = document.createElement("a")
+        a.href = canvas.toDataURL("image/png")
+        a.download = `rarity-bg-${scaling}.png`
+        a.dispatchEvent(new MouseEvent("click"))
+    }
 
-gen(1)
-gen(2)
+    for (let i = 1; i < 3; i += 0.5) {
+        gen(i)
+        await new Promise(resolve => setTimeout(resolve, 200))
+    }
+})();
